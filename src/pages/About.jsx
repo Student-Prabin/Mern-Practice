df35bbc41f5c6311f7081171f0fe917cec47ecb8
Student-Prabin <dahalprabin74@gmail.com>
2025-06-18 17:28:14 +0545

    small fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
           <h1 className="text-4xl">About Catering</h1>
           <p className="text-xl">Tradition since 1889</p>
 
-          <p className="">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use
+          <p className="">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use&nbsp;
             <span className="bg-gray-200">seasonal</span> ingredients.</p>
 
           <p className="opacity-80">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
