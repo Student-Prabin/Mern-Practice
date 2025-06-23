7fffb2dd92b38130f7d4442f1e6e9fd4435710e1
Student-Prabin <dahalprabin74@gmail.com>
2025-06-23 21:33:23 +0545

    Some more useState practice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,44 @@
 import { faker } from '@faker-js/faker';
-import { Button } from '@material-tailwind/react';
-import {
-  List,
-  ListItem,
-  ListItemSuffix,
-  Chip,
-  Card,
-} from "@material-tailwind/react";
 import { useState } from 'react';
+import { Button } from '@material-tailwind/react';
 
 export default function Home() {
+  const [toggle, setToggle] = useState(true);
+  const [count, setCount] = useState(0);
+  const [avatar, setAvatar] = useState('');
+  const [randomEmail, setEmail] = useState('');
 
-  const [data, setData] = useState([]);
+  const handleColor = () => {
+    setToggle(!toggle);
+  };
 
-  const handleClick = () => {
-    setData((prev) => [...prev, faker.science.chemicalElement()]);
-  }
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
 
-  const handleDelete = (dlt) => {
-    setData((prev) => prev.filter((_, index) => index !== dlt));
-  }
+  const generateRandomUser = () => {
+    const fakeAvatar = faker.image.avatar();
+    const fakeEmail = faker.internet.email();
+    setAvatar(fakeAvatar);
+    setEmail(fakeEmail);
+  };
 
   return (
-    <div className='p-5'>
+    <div className={`p-5 min-h-screen transition-colors duration-300`}>
+      <Button color={`${toggle ? 'red' : 'black'}`} onClick={handleColor}>Toggle Color</Button>
+      <br /><br />
 
-      <Button onClick={handleClick} className='cursor-pointer'> Add Fake Data</Button>
+      <Button onClick={increment}>Increment</Button>
+      <h1 className="text-2xl my-2">{count}</h1>
+      <Button onClick={decrement}>Decrement</Button>
+      <br /><br />
 
-      {data.length > 0 && <Card className="w-96 mt-6">
-        <List>
-          {data.map((chemical, index) => {
-            return <ListItem key={index}>
-              {chemical.name}
-              <ListItemSuffix className='flex gap-5'>
-                <Chip
-                  value={chemical.symbol}
-                  variant="ghost"
-                  size="sm"
-                  className="rounded-full"
-                />
-                <Button size='sm' className='text-[10px]' color='pink' onClick={() => handleDelete(index)}>Delete</Button>
-              </ListItemSuffix>
-            </ListItem>
-          })}
-
-
-        </List>
-      </Card>
-      }
-
-
+      <Button color="purple" onClick={generateRandomUser}>Create Random Image</Button>
+      {avatar && (
+        <div className="mt-4">
+          <img src={avatar} alt="Random Avatar" className="rounded-full w-32 h-32 mx-auto" />
+          <p className="text-center mt-2">{randomEmail}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
