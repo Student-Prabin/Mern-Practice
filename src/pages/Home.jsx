de56fbeb2e1e14ebf12222f52a78114191e1d520
Student-Prabin <dahalprabin74@gmail.com>
2025-06-29 08:02:16 +0545

    Started working with apis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,32 @@
-import { faker } from '@faker-js/faker';
-import { useState } from 'react';
-import { Button } from '@material-tailwind/react';
+import axios from "axios"
+import { useState } from "react";
 
 export default function Home() {
-  const [toggle, setToggle] = useState(true);
-  const [count, setCount] = useState(0);
-  const [avatar, setAvatar] = useState('');
-  const [randomEmail, setEmail] = useState('');
 
-  const handleColor = () => {
-    setToggle(!toggle);
-  };
+  // const m = axios.get('https://jsonplaceholder.typicode.com/posts');
+  // console.log(m)
+  const [msg, setMsg] = useState("");
 
-  const increment = () => setCount((prev) => prev + 1);
-  const decrement = () => setCount((prev) => prev - 1);
+  const mi = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success bhayo')
+    }, 2000);
+  });
 
-  const generateRandomUser = () => {
-    const fakeAvatar = faker.image.avatar();
-    const fakeEmail = faker.internet.email();
-    setAvatar(fakeAvatar);
-    setEmail(fakeEmail);
-  };
+  mi.then((val) => {
+    console.log(`Success: ${val}`);
+    setMsg(val);
+  }).catch((err) => {
+    console.log(`Rejected: ${err}`);
+    setMsg(err);
+  })
+  // .finally(() => {
+  //   console.log('hello');
+  // })
 
   return (
-    <div className={`p-5 min-h-screen transition-colors duration-300`}>
-      <Button color={`${toggle ? 'red' : 'black'}`} onClick={handleColor}>Toggle Color</Button>
-      <br /><br />
-
-      <Button onClick={increment}>Increment</Button>
-      <h1 className="text-2xl my-2">{count}</h1>
-      <Button onClick={decrement}>Decrement</Button>
-      <br /><br />
-
-      <Button color="purple" onClick={generateRandomUser}>Create Random Image</Button>
-      {avatar && (
-        <div className="mt-4">
-          <img src={avatar} alt="Random Avatar" className="rounded-full w-32 h-32 mx-auto" />
-          <p className="text-center mt-2">{randomEmail}</p>
-        </div>
-      )}
+    <div className="p-10">
+      <h1>{msg}</h1>
     </div>
-  );
+  )
 }
