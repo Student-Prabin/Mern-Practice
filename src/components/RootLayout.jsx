ef6234d31f9842702370fa3696d0d7110b943484
Student-Prabin <dahalprabin74@gmail.com>
2025-07-11 08:13:06 +0545

    new hooks

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router";
+import { Outlet, useSearchParams } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
+import { Button } from "@material-tailwind/react";
 
 export default function RootLayout() {
+
   return (
     <>
       <Header />
